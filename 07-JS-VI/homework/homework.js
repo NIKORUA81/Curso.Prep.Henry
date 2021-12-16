15066472c94f967162cc8674a4055f5411aef480
Nicolas Rua <nikorua81@gmail.com>
2021-12-15 22:50:13 -0500

    Guardado 15-12

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -22,12 +22,21 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma = 0;
+
+  for (var i = 0; i < numeros.length; i++) {
+    suma = suma + numeros[i];    
+  }
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  for (var i = 0; i < array.length; i++){
+    cb(array[i])
+  }
 }
 
 function map(array, cb) {
@@ -35,12 +44,27 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  
+  var newArray = []
+  for (var i = 0; i<array.length; i++){
+    newArray.push(cb(array[i]))
+  }
+  return newArray
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var newArray = []
+
+  for(var i = 0; i < array.length; i++){
+    if (array[i][0] === 'a') {
+      newArray.push(array[i])    
+    }
+  }
+
+  return newArray
 }
 
 // No modificar nada debajo de esta línea
